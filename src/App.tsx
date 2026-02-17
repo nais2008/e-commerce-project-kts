aedc30824743a0629b20c70016df42a43042c71d
nais2008 <t09n09a08@gmail.com>
2026-02-17 18:31:54 +0500

    remove: react logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react"
 
 import "./App.css"
-import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 
 function App() {
@@ -12,9 +11,6 @@ function App() {
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
